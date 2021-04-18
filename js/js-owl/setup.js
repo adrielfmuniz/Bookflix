1e73d346e6208b0179d939e4c2121af4477de223
unknown <adriel@gmx.com>
2021-04-18 05:46:48 -0300

    Projeto Bookflix

---
+++ js/js-owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 7,
+    nav: false,
+    dots: false,
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+function mais_info() {
+    alert("Livro premiado no mundo todo. Com certeza você irá repensar suas finanças. Iniciei essa jornada agora mesmo!");
+}
+
+function video_url() {
+    window.open("https://www.youtube.com/watch?v=USwqPoFfgfw&list=WL&index=2&t=10s");
+}
+
+function trocar(elemento) {
+    elemento.innerHTML = "Ver tudo"
+}
+
+function voltar_acess(elemento) {
+    elemento.innerHTML = "Mais Acessados"
+}
+
+function voltar_invest_fin(elemento) {
+    elemento.innerHTML = "Investimentos e Finanças"
+}
+
+function voltar_gest_lid(elemento) {
+    elemento.innerHTML = "Gestão e Liderança"
+}
